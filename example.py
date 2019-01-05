263ebc3997ce30da14588255f3b48a3545048dd5
linsicheng20060818 <37499324+linsicheng20060818@users.noreply.github.com>
2019-01-05 22:16:25 +0800

    Update example.py

--- example.py
+++ example.py
@@ -5,9 +5,8 @@ Created on Sat Jan  5 20:58:40 2019
 
 @author: lin
 """
-'''from pyaibot import chchat
-while True:
-    l=input(">")
-    print(chchat(l))'''
+from pyaibot import chchat
+l=input(">")
+print(chchat(l))'''#pychatbot
 from Enfc import Enfc0
-print(Enfc0(input(">")))
+print(Enfc0(input(">")))#Enfc
