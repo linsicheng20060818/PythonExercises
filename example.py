5ee89ba4f180e58d14da5c3510981114d0b047bd
lin <3083682067@qq.com>
2019-01-06 14:58:33 +0800

    debug

--- example.py
+++ example.py
@@ -5,9 +5,10 @@ Created on Sat Jan  5 20:58:40 2019
 
 @author: lin
 """
+
 from pyaibot import chchat
 l=input(">")
 print(chchat(l))#pychatbot
 
-from Enfc import Enfc0
-print(Enfc0(input(">")))#Enfc
+import Enfc
+print(Enfc.Enfc0(input(">")))#Enfc0
