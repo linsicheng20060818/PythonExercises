c8aab16248c21376f3d4f03fe8d5941e72988277
lin <3083682067@qq.com>
2019-01-06 20:42:20 +0800

    rm Enfc

--- Enfc.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def Enfc0(a):
-    import jieba
-    list1=jieba.lcut(a)
-    list2=[]
-    for i in range(len(list1)):
-        if list1[i]!=" ":
-            list2.append(list1[i])
-    return(list2)
