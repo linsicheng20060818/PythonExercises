a3cb97e5f49ddb48bd08adcb5c524f983dddb6e0
linsicheng20060818 <37499324+linsicheng20060818@users.noreply.github.com>
2019-01-20 19:51:32 +0800

    a
    pyaibot.py--done!

--- pyaibot.py
+++ pyaibot.py
@@ -15,24 +15,23 @@ def chchat(a):
     #print(list1)
     i=0
     b=""
-    r=0
+  
     if list1[i]=="你好":#如果是打招呼，直接输出
        return(a)
     else:
-        ni=[]
-        wo=[]
+        
         for i in range(len(list1)):#判断 '你'和'我'
             if list1[i]=="你":
-                 ni.append(i)
-            if list1[i]=="我":
-                 wo.append(i)
-            if list1[i]=="几":
+                list1[i]="我"
+            elif list1[i]=="我":
+                 list1[i]="你"
+            elif list1[i]=="几":
                 import random
                 v=True
-        for r in range(len(ni)):#'你'换成'我'
+        '''for r in range(len(ni)):#'你'换成'我'
             list1[r]="我"
         for i in range(len(wo)):#'我'换成'你'
-            list1[i]="你"
+            list1[i]="你"'''
         for i in range(len(list1)):
             b=b+list1[i]
         if v==True:
